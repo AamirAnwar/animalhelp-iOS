9f1b34bf92222aa0ae624563e824bf1aa282ffaf
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-01-04 19:00:08 +0530

    Added table view to account page with dummy data

--- animalhelp/animalhelp/Account Page/AccountViewController.swift
+++ animalhelp/animalhelp/Account Page/AccountViewController.swift
@@ -9,6 +9,10 @@
 import Foundation
 import UIKit
 class AccountViewController:BaseViewController {
+    let cellIdentifier = "AccountViewControllerCell"
+    let tableView = UITableView(frame: CGRect.zero, style: .plain)
+    let accountItems = ["Profile", "Pet Lookout" ,"Push Notifications", "Email Alerts", "Terms and Conditions", "Feedback", "How to help", "Logout"]
+    
     init() {
         super.init(nibName: nil, bundle: nil)
         self.tabBarItem.title = "Account"
@@ -20,5 +24,29 @@ class AccountViewController:BaseViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.addSubview(self.tableView)
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.tableFooterView = UIView(frame:CGRect.zero)
+        self.tableView.snp.makeConstraints { (make) in
+            make.top.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+        }
+    }
+}
+
+extension AccountViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return accountItems.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)!
+        cell.textLabel?.text = accountItems[indexPath.row]
+        return cell
+        
     }
 }
