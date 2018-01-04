7a03e64551e0ab1c653952e8f70857b7a1aa1234
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-01-04 21:44:06 +0530

    Drawer View,CustomFonts, Custom Colors and Location Pin Image
    - Created a drawer view for the home page (v1). Also added files for holding colors and fonts used for the project.

---
+++ animalhelp/animalhelp/Commons/CustomFonts.swift
@@ -0,0 +1,9 @@
+//
+//  CustomFonts.swift
+//  animalhelp
+//
+//  Created by Aamir  on 04/01/18.
+//  Copyright © 2018 AamirAnwar. All rights reserved.
+//
+
+import Foundation
---
+++ animalhelp/animalhelp/Home Page/DrawerView.swift
@@ -0,0 +1,9 @@
+//
+//  DrawerView.swift
+//  animalhelp
+//
+//  Created by Aamir  on 04/01/18.
+//  Copyright © 2018 AamirAnwar. All rights reserved.
+//
+
+import Foundation
