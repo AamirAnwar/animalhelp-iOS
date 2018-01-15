7ead4e4efef2be74048bbd03590e779c3c4c08b0
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-01-16 03:51:07 +0530

    Added new font for custom nav bar title

--- animalhelp/animalhelp/Commons/CustomFonts.swift
+++ animalhelp/animalhelp/Commons/CustomFonts.swift
@@ -13,3 +13,4 @@ let CustomFontBodyMedium = UIFont(name: "AvenirNext-Medium", size: 18)!
 let CustomFontSmallBodyMedium = UIFont(name: "AvenirNext-Medium", size: 14)!
 let CustomFontButtonTitle = UIFont(name: "AvenirNext-DemiBold", size: 19)!
 let CustomFontHeadingSmall = UIFont(name: "AvenirNext-DemiBold", size: 14)!
+let CustomFontTitleBold = UIFont(name: "AvenirNext-Bold", size: 26)!
