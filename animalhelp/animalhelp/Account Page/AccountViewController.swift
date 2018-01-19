a6d5f90562e4f09119ccb5e0d5eac024bf4d2437
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-01-19 14:08:37 +0530

    Account page guest UI complete

--- animalhelp/animalhelp/Account Page/AccountViewController.swift
+++ animalhelp/animalhelp/Account Page/AccountViewController.swift
@@ -12,42 +12,36 @@ import GoogleSignIn
 import FacebookLogin
 
 class AccountViewController:BaseViewController,GIDSignInUIDelegate {
-    let cellIdentifier = "AccountViewControllerCell"
+    let kLoginCellReuseIdentifier = "SocialLoginTableViewCell"
+    let kStandardListCellReuseIdentifier = "StandardListTableViewCell"
+    let standardAccountItems = ["Push Notifications", "Terms and Conditions", "Feedback", "How to help"]
     let tableView = UITableView(frame: CGRect.zero, style: .plain)
-    let accountItems = ["Profile", "Pet Lookout" ,"Push Notifications", "Email Alerts", "Terms and Conditions", "Feedback", "How to help", "Logout"]
-    let googleSignInButton = GIDSignInButton()
-    let fbLoginButton = LoginButton(readPermissions: [.publicProfile])
+    var accountItems:[String] {
+        get {
+            if loginManager.isLoggedIn {
+                return  ["Pet Lookout", "Logout"] + standardAccountItems
+            }
+            return standardAccountItems
+        }
+    }
+    let loginManager = LoginManager.sharedInstance
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        googleSignInButton.style = GIDSignInButtonStyle(rawValue: 1)!
-        GIDSignIn.sharedInstance().uiDelegate = self
-
         self.tabBarItem.title = "Account"
         self.customNavBar.setTitle("Account")
         self.customNavBar.disableLocationButton()
         self.view.addSubview(self.tableView)
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: self.cellIdentifier)
+        // Register Cell Types
+        self.tableView.register(SocialLoginTableViewCell.self, forCellReuseIdentifier: self.kLoginCellReuseIdentifier)
+        self.tableView.register(StandardListTableViewCell.self, forCellReuseIdentifier: self.kStandardListCellReuseIdentifier)
         self.tableView.delegate = self
         self.tableView.dataSource = self
         self.tableView.tableFooterView = UIView(frame:CGRect.zero)
-        
-        self.view.addSubview(self.googleSignInButton)
-        self.googleSignInButton.snp.makeConstraints { (make) in
-            make.leading.equalToSuperview().offset(10)
-            make.trailing.equalToSuperview().inset(10)
-            make.top.equalTo(self.customNavBar.snp.bottom)
-        }
-        
-        self.view.addSubview(self.fbLoginButton)
-        self.fbLoginButton.snp.makeConstraints { (make) in
-            make.leading.equalTo(self.googleSignInButton)
-            make.trailing.equalTo(self.googleSignInButton)
-            make.height.equalTo(self.googleSignInButton.snp.height)
-            make.top.equalTo(self.googleSignInButton.snp.bottom).offset(10)
-        }
+        self.tableView.separatorStyle = .none
+   
         self.tableView.snp.makeConstraints { (make) in
-            make.top.equalTo(self.fbLoginButton.snp.bottom)
+            make.top.equalTo(self.customNavBar.snp.bottom)
             make.bottom.equalToSuperview()
             make.left.equalToSuperview()
             make.right.equalToSuperview()
@@ -56,14 +50,41 @@ class AccountViewController:BaseViewController,GIDSignInUIDelegate {
 }
 
 extension AccountViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return accountItems.count
+        if section == 0 {
+            return 1
+        }
+        else {
+            return accountItems.count
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier)!
-        cell.textLabel?.text = accountItems[indexPath.row]
+        var cell:UITableViewCell!
+        if indexPath.section == 0 {
+            cell = tableView.dequeueReusableCell(withIdentifier: kLoginCellReuseIdentifier) as! SocialLoginTableViewCell
+            cell.showBottomPaddedSeparator()
+        }
+        else {
+            let standardCell = tableView.dequeueReusableCell(withIdentifier: kStandardListCellReuseIdentifier) as! StandardListTableViewCell
+            standardCell.setTitle(accountItems[indexPath.row])
+            standardCell.tintColor = CustomColorTextBlack
+            if indexPath.row < self.accountItems.count - 1 {
+                standardCell.showBottomPaddedSeparator()
+            }
+            cell = standardCell
+            
+        }
         return cell
-        
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
