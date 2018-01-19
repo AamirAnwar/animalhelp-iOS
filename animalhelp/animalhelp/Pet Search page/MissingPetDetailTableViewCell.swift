5380e3e8dbc9aa9a854859f82dcb07c1e8005ad9
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-01-20 01:39:41 +0530

    Implemented pet detail cell

--- animalhelp/animalhelp/Pet Search page/MissingPetDetailTableViewCell.swift
+++ animalhelp/animalhelp/Pet Search page/MissingPetDetailTableViewCell.swift
@@ -7,3 +7,53 @@
 //
 
 import Foundation
+import UIKit
+class MissingPetDetailTableViewCell:UITableViewCell {
+    let titleLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = CustomFontHeadingSmall
+        label.textColor = CustomColorTextBlack
+        return label
+    }()
+
+    let subtitleLabel:UILabel = {
+        let label = UILabel()
+        label.numberOfLines = 0
+        label.font = CustomFontSmallBodyMedium
+        label.textColor = CustomColorLightGray
+        return label
+    }()
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Init with coder not implemented")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(self.titleLabel)
+        self.contentView.addSubview(self.subtitleLabel)
+        self.selectionStyle = .none
+        self.titleLabel.snp.makeConstraints { (make) in
+            make.top.equalToSuperview().offset(8)
+            make.leading.equalToSuperview().offset(kSidePadding)
+            make.trailing.equalToSuperview().inset(kSidePadding)
+        }
+        
+        self.subtitleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(2)
+            make.leading.equalTo(self.titleLabel.snp.leading)
+            make.trailing.equalTo(self.titleLabel.snp.trailing)
+            make.bottom.equalToSuperview().inset(8)
+        }
+    }
+    
+    func setTitle(_ title:String, subtitle:String) {
+        self.titleLabel.text = title
+        self.subtitleLabel.text = subtitle
+    }
+    
+    func setSubtitleTextColor(_ color:UIColor) {
+        self.subtitleLabel.textColor = color
+    }
+}
