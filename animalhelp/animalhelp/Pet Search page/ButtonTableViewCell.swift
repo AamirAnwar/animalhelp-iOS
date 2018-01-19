43ab630abca4411a02719ddc084ce1ba1f3d55cf
Aamir Anwar <aamir.anwar021193@gmail.com>
2018-01-20 01:40:01 +0530

    Created a cell with a customizable button within it

--- animalhelp/animalhelp/Pet Search page/ButtonTableViewCell.swift
+++ animalhelp/animalhelp/Pet Search page/ButtonTableViewCell.swift
@@ -7,3 +7,37 @@
 //
 
 import Foundation
+import UIKit
+
+class ButtonTableViewCell:UITableViewCell {
+    let button = UIButton(type: .system)
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init with coder not implemented")
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.contentView.addSubview(self.button)
+        
+        self.button.titleLabel?.textAlignment = .center
+        self.button.titleLabel?.font = CustomFontUsername
+        button.layer.cornerRadius = kCornerRadius
+        self.button.snp.makeConstraints { (make) in
+            make.leading.equalToSuperview().offset(kSidePadding)
+            make.trailing.equalToSuperview().inset(kSidePadding)
+            make.top.equalToSuperview().offset(8)
+            make.bottom.equalToSuperview().inset(8)
+        }
+    }
+    
+    public func setButtonTitle(_ title:String) {
+        self.button.setTitle(title, for: .normal)
+    }
+    
+    public func setButtonTint(_ tint:UIColor) {
+        self.button.setTitleColor(tint, for: .normal)
+        self.button.layer.borderColor = tint.cgColor
+        self.button.layer.borderWidth = 1
+    }
+}
